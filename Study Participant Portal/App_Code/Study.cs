140adb851b7ea07080c0b3a07ddd521b104e7d13
Max Mueller <muellmax36@gmail.com>
2012-02-14 00:22:31 -0800

    added basic create study page, and added more to the research page. Database functionality to be added very soon.
    MM

---
+++ Study Participant Portal/App_Code/Study.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Study
+/// </summary>
+public class Study
+{
+    private string studyName;
+    private string studyDescript;
+    private bool expired;
+    private string dateCreated;
+
+    public string StudyName {
+        get { return studyName; }
+        set { studyName = value; }
+    }
+
+    public string StudyDescript {
+        get { return studyDescript; }
+        set { studyDescript = value; }
+    }
+
+    public bool Expired {
+        get { return expired; }
+        set { expired = value; }
+    }
+
+    public string DateCreated {
+        get { return dateCreated; }
+        set { dateCreated = value; }
+    }
+
+	public Study()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
