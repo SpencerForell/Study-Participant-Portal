0ae0cd56eb1a2ca4e5a19904e71b71221fe0cde2
Spencer Forell <spencerforell@gmail.com>
2012-02-15 20:02:24 -0800

    Made participants constructor to create object by using user ID and querying database. Also changed the datecreated to a datetime field

--- Study Participant Portal/App_Code/Study.cs
+++ Study Participant Portal/App_Code/Study.cs
@@ -6,33 +6,46 @@ using System.Web;
 /// <summary>
 /// Summary description for Study
 /// </summary>
-public class Study
-{
+public class Study {
     private string studyName;
-    private string studyDescript;
+    private string studyDescription;
+    private DateTime dateCreated;
     private bool expired;
-    private string dateCreated;
+    private int study_id;
+    private int researcher_id;
 
     public string StudyName {
         get { return studyName; }
     }
 
-    public string StudyDescript {
-        get { return studyDescript; }
+    public string StudyDescription {
+        get { return studyDescription; }
+    }
+    
+    public DateTime DateCreated {
+        get { return dateCreated; }
     }
 
     public bool Expired {
         get { return expired; }
     }
 
-    public string DateCreated {
-        get { return dateCreated; }
+    public int Study_ID {
+        get { return study_id; }
     }
 
-	public Study()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    public int Researcher_ID {
+        get { return researcher_id; }
+    }
+
+    public Study(int study_id) {
+        string queryString = "select name, Description, Creation_Date, Expired, Res_ID from Study where Study_ID = " + study_id.ToString();
+        DatabaseQuery query = new DatabaseQuery(queryString, DatabaseQuery.Type.Select);
+        this.studyName = query.Results[0][0];
+        this.studyDescription = query.Results[0][1];
+        this.dateCreated = Convert.ToDateTime(query.Results[0][2]);
+        this.expired = Convert.ToBoolean(Convert.ToInt32(query.Results[0][3]));
+        this.researcher_id = Convert.ToInt32(query.Results[0][4]);
+        this.study_id = study_id;
+    }
 }
