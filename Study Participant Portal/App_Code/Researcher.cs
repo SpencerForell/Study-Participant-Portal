de5810817e5f2e518da8bfcb681fdef1e1e57d85
Spencer Forell <spencerforell@gmail.com>
2012-02-15 20:01:22 -0800

    Added constructor so that researcher objects can be created by using their user_id and querying the database

--- Study Participant Portal/App_Code/Researcher.cs
+++ Study Participant Portal/App_Code/Researcher.cs
@@ -11,8 +11,9 @@ public class Researcher: SuperUser {
     /// <summary>
     /// Constructor that builds a researcher object by passing in all of the attributes
     /// </summary>
-    public Researcher(string user_name, string first_name, string last_name, string email) {
+    public Researcher(int user_id, string user_name, string first_name, string last_name, string email) {
         type = UserType.Researcher;
+        this.user_id = user_id;
         this.user_name = user_name;
         this.first_name = first_name;
         this.last_name = last_name;
@@ -24,7 +25,13 @@ public class Researcher: SuperUser {
     /// </summary>
     /// <param name="user_id">The id of the researcher to create an object for</param>
     public Researcher(int user_id) {
-        //do query and set all fields to query results
-        //note*** need to make sure the user_id exists
+        string queryString = "select User_Name, First_Name, Last_Name, Email from Researcher where Res_ID = " + user_id.ToString();
+        DatabaseQuery query = new DatabaseQuery(queryString, DatabaseQuery.Type.Select);
+
+        this.user_id = user_id;
+        this.user_name = query.Results[0][0];
+        this.first_name = query.Results[0][1];
+        this.last_name = query.Results[0][2];
+        this.email = query.Results[0][3];
     }
 }
