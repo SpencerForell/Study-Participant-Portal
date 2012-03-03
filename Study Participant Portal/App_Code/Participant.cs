c0f2816592330dbfa0fe1ea6e99beeef39cc3b24
Spencer Forell <spencerforell@gmail.com>
2012-03-03 13:15:09 -0800

    Fixed constructor to take in the user_id

--- Study Participant Portal/App_Code/Participant.cs
+++ Study Participant Portal/App_Code/Participant.cs
@@ -11,8 +11,9 @@ public class Participant: SuperUser {
     /// <summary>
     /// Constructor that builds a researcher object by passing in all of the attributes
     /// </summary>
-    public Participant(string user_name, string first_name, string last_name, string email) {
+    public Participant(int userID, string user_name, string first_name, string last_name, string email) {
         type = UserType.Participant;
+        this.userID = userID;
         this.userName = user_name;
         this.firstName = first_name;
         this.lastName = last_name;
@@ -24,7 +25,13 @@ public class Participant: SuperUser {
     /// </summary>
     /// <param name="user_id">The id of the researcher to create an object for</param>
     public Participant(int user_id) {
-        //do query and set all fields to query results
-        //note*** need to make sure the user_id exists
+        string queryString = "select User_Name, First_Name, Last_Name, Email from Participant where Par_ID = " + user_id.ToString();
+        DatabaseQuery query = new DatabaseQuery(queryString, DatabaseQuery.Type.Select);
+
+        this.userID = user_id;
+        this.userName = query.Results[0][0];
+        this.firstName = query.Results[0][1];
+        this.lastName = query.Results[0][2];
+        this.email = query.Results[0][3];
     }
 }
