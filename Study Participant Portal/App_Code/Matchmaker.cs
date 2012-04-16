35c834a2db02d7e795cd57c8fee01074f88560d2
Spencer Forell <spencerforell@gmail.com>
2012-04-15 21:52:40 -0700

    Created new file for matchmaker class

---
+++ Study Participant Portal/App_Code/Matchmaker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Matchmaker
+/// </summary>
+public class Matchmaker {
+	public Matchmaker(Study study) {
+		
+	}
+}
