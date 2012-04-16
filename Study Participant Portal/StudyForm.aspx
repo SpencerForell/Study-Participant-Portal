f5e33e910bbb4f079b5212fb75721e082fab92b3
Spencer Forell <spencerforell@gmail.com>
2012-04-15 21:52:12 -0700

    Created dynamic Study Form for the researcher side of viewing a study

--- Study Participant Portal/StudyForm.aspx
+++ Study Participant Portal/StudyForm.aspx
@@ -3,14 +3,20 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
-    <asp:Label ID="lblStdName" runat="server" Text="Study name goes here"></asp:Label>
-    <br />
-    <br />
-    <asp:Label ID="lblStdCreator" runat="server" Text="Study Creator Name goes here"></asp:Label>
-    <br />
-    <asp:Label ID="lblStdDate" runat="server" Text="Study date created goes here"></asp:Label>
-    <br />
-    <asp:Textbox ID="tbStdDescription" runat="server" Text="Study description goes here" TextMode="MultiLine"></asp:Textbox>
+    <asp:Panel ID="pnlStudyDetails" runat="server">
+        <asp:Label ID="lblStdName" runat="server" Text="Study name goes here"></asp:Label>
+        <br />
+        <asp:Label ID="lblStdDate" runat="server" Text="Study date created goes here"></asp:Label>
+        <br />
+        <asp:Textbox ID="tbStdDescription" runat="server" Text="Study description goes here" TextMode="MultiLine" Enabled="false"></asp:Textbox>
+
+        <asp:Panel ID="pnlStdQualifiers" runat="server">
+            <asp:Label ID="lblQualifiers" runat="server" Text="Qualifiers"></asp:Label>
+        </asp:Panel>
+    </asp:Panel>
+
+    <asp:Button ID="btnFindParticipants" runat="server" Text="Find Participants" 
+        onclick="btnFindParticipants_Click" />
 
 </asp:Content>
 
--- Study Participant Portal/StudyForm.aspx.cs
+++ Study Participant Portal/StudyForm.aspx.cs
@@ -6,17 +6,52 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class StudyForm : System.Web.UI.Page {
+    Study study;
+    Researcher researcher;
+
     protected void Page_Load(object sender, EventArgs e) {
-        int study_id = Convert.ToInt32(Request.QueryString["study_id"]);
-        Study study = new Study(study_id);
+        if (!IsPostBack) {
+            int studyID = Convert.ToInt32(Request.QueryString["study_id"]);
+            study = new Study(studyID);
+            researcher = new Researcher(study.ResearcherID);
+            lblStdName.Text = study.Name;
+            lblStdDate.Text = study.DateCreated.ToString();
+            tbStdDescription.Text = study.Description;
 
-        Researcher res = new Researcher(study.ResearcherID);
+            generateQualifiers(study);
+        }
+    }
 
-        lblStdName.Text = study.Name;
-        lblStdCreator.Text = res.FirstName + " " + res.LastName;
-        lblStdDate.Text = study.DateCreated.ToString();
-        tbStdDescription.Text = study.Description;
-        tbStdDescription.ReadOnly = true;
+    /// <summary>
+    /// Dynamically creates panels that store each qualifier and adds them to the main panel of the page.
+    /// </summary>
+    /// <param name="study"></param>
+    private void generateQualifiers(Study study) {
+        foreach (Qualifier qualifier in study.Qualifiers) {
+            //create a new panel that will hold all the information for this qualifier
+            Panel pnlQualifer = new Panel();
+            LiteralControl lineBreak = new LiteralControl("<br>");
+            Label lblQualifier = new Label();
+            RadioButtonList rblistAnswers = new RadioButtonList();
+            foreach (Answer answer in qualifier.Answers) {
+                rblistAnswers.Items.Add(answer.AnswerText);
+            }
+            pnlQualifer.Controls.Add(lineBreak);
+            lblQualifier.Text = qualifier.Question;
+            pnlQualifer.Controls.Add(lblQualifier);
+            rblistAnswers.Enabled = false;
+            pnlQualifer.Controls.Add(rblistAnswers);
+            //add the panel we just made to the form
+            pnlStdQualifiers.Controls.Add(pnlQualifer); 
+        }
+    }
+
+    /// <summary>
+    /// Event triggered to perform the matchmaking algorithm
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void btnFindParticipants_Click(object sender, EventArgs e) {
         
     }
 }
