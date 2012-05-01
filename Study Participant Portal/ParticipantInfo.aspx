893d105ad530b2328817e4fb0d7b061aa31daeca
Spencer Forell <spencerforell@gmail.com>
2012-05-01 14:55:20 -0700

    Added ability to drill down into a particular users information and view extra details, questions/answers about them

---
+++ Study Participant Portal/ParticipantInfo.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="ParticipantInfo.aspx.cs" Inherits="ParticipantInfo" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <table style="width:100%">
+        <tr>
+            <td style="width:50%">
+                <asp:Panel ID="pnlParticipantInfo" runat="server" GroupingText="Participant Info">
+                    <asp:Label ID="lblFullName1" runat="server" Text="Name:" CssClass="label" ></asp:Label>
+                    <asp:Label ID="lblFullName2" runat="server" ></asp:Label><br />
+                    <asp:Label ID="lblUserName1" runat="server" Text="User Name:" CssClass="label" ></asp:Label>
+                    <asp:Label ID="lblUserName2" runat="server" ></asp:Label><br />
+                    <asp:Label ID="lblEmail1" runat="server" Text="Email:" CssClass="label" ></asp:Label>
+                    <asp:Label ID="lblEmail2" runat="server" ></asp:Label><br /><br />
+                    <asp:Button ID="btnShowAll" runat="server" 
+                        Text="Show Questions & Answers for all studies" onclick="btnShowAll_Click" /><br />
+                    <asp:Button ID="btnShowThisStudy" runat="server" 
+                        Text="Show Questions and Answers for this study" 
+                        onclick="btnShowThisStudy_Click" /><br />
+                </asp:Panel>    
+            </td>
+            <td >
+               <asp:Panel ID="pnlQualifiers" runat="server" GroupingText="Questions/Answers"></asp:Panel>
+            </td>
+        </tr>
+    
+    </table>
+
+
+</asp:Content>
+
---
+++ Study Participant Portal/ParticipantInfo.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ParticipantInfo : System.Web.UI.Page {
+
+    
+    protected void Page_Load(object sender, EventArgs e) {
+        if (!IsPostBack) {
+            Study study = new Study(Convert.ToInt32(Request.QueryString["study_id"]));
+            Participant participant = new Participant(Convert.ToInt32(Request.QueryString["participant_id"]));
+            lblUserName2.Text = participant.UserName;
+            lblFullName2.Text = participant.FirstName + " " + participant.LastName;
+            lblEmail2.Text = participant.Email;
+            showStudyInfo(study, participant);
+
+        }
+    }
+
+
+
+    /// <summary>
+    /// Shows questions/answers for all existing studies
+    /// </summary>
+    private void showAllInfo(Participant participant) {
+        List<Study> studies = DAL.GetStudies();
+        foreach (Study study in studies) {
+            showStudyInfo(study, participant);
+        }
+    }
+
+    /// <summary>
+    /// Shows questions/answers and score for a particular study
+    /// </summary>
+    /// <param name="study"></param>
+    private void showStudyInfo(Study study, Participant participant) {
+        foreach (Qualifier qualifier in study.Qualifiers) {
+            Label lblQualifier = new Label();
+            pnlQualifiers.Controls.Add(lblQualifier);
+            RadioButtonList rblistAnswers = new RadioButtonList();
+            foreach (Answer answer in qualifier.Answers) {
+                rblistAnswers.Items.Add(answer.AnswerText);
+                foreach (Answer participantAnswer in participant.Answers) {
+                    if (answer.AnsID == participantAnswer.AnsID) {
+                        rblistAnswers.SelectedValue = answer.AnswerText;
+                    }
+                }
+            }
+            pnlQualifiers.Controls.Add(rblistAnswers);
+            lblQualifier.Text = qualifier.Question;
+            rblistAnswers.Enabled = false;
+        }        
+    }
+
+    /// <summary>
+    /// Shows all of the questions/answers for the user
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void btnShowAll_Click(object sender, EventArgs e) {
+        Participant participant = new Participant(Convert.ToInt32(Request.QueryString["participant_id"]));
+        pnlQualifiers.Controls.Clear();
+        showAllInfo(participant);
+    }
+
+    /// <summary>
+    /// Shows only the quuestions and answers for the current study being viewed
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void btnShowThisStudy_Click(object sender, EventArgs e) {
+        Study study = new Study(Convert.ToInt32(Request.QueryString["study_id"]));
+        Participant participant = new Participant(Convert.ToInt32(Request.QueryString["participant_id"]));
+        pnlQualifiers.Controls.Clear();
+        showStudyInfo(study, participant);
+    }
+}
